923e31463721c544c58b6bf1f7aa3911e3b91d92
Ross Ning <252551516@qq.com>
2016-12-15 11:24:35 -0500

    1. add initRouters() to initialize all routers from file 2. add infile.dat

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <list>
 #include <map>
+#include <sstream>
 
 using namespace std;
 #define INFTY 2147483647;
@@ -29,24 +30,52 @@ public:
     }
 };
 
+class ConnectedRouter
+{
+public:
+	int RouterID;
+	int Cost;
+	
+	ConnectedRouter(int routerId, int cost)
+		: RouterID(routerId), Cost(cost) {}
+};
+
 class Router{
 public:
     int ID;
     string Network;
     int Cost;
     int LSPNum;
+    vector<ConnectedRouter> ConnectedRouters;
+    
+
+    // TODO: we don't need this
     vector <Router> ConnectedNetwork;
+
+    
     list<pair<int, int>>NetworkGraph;
     map<int,int> ReceivedLSP;
+    
+    
+    // init a router with invalid values
+    // must declare this function for std::map
+    Router() {
+    	ID = -1;
+    	Cost = -1;
+    	LSPNum = 0;
+	}
+    
     Router(int id,string net,int cost){
         ID=id;
         Network=net;
         Cost=cost;
         LSPNum=0;
     }
+    
     void AddNum(){
         LSPNum=LSPNum+1;
     }
+    
     void ReceiveLSP(LSP lsp){
         LSP t=lsp;
         t.TTL--;
@@ -57,6 +86,7 @@ public:
             }
         }
     }
+    
     LSP OriginateLSP(){
         AddNum();
         LSP lsp(ID,LSPNum);
@@ -68,10 +98,79 @@ public:
     
 };
 
+map<int, Router> g_routers;
 
-
+void initRouters(map<int, Router>& routers) {
+	
+	Router router;
+	
+	ifstream ifs("infile.dat");
+	string line;
+	while (!ifs.eof()) {
+		getline(ifs, line);
+		//cout << line << endl;
+		
+		
+		if (line[0] != ' ') {
+			
+			// add router to router list
+			if (router.ID >= 0) {
+				routers[router.ID] = router;
+			}
+			
+			// create new router
+			router = Router();
+			
+			stringstream ss(line);
+			
+			int routerId = -1;
+			string networkName;
+			int cost = 1; // default value of cost
+			
+			string word;
+			while (ss >> word) {
+				if (routerId < 0) {
+					routerId = atoi(word.c_str());
+				} else if (networkName.empty()) {
+					networkName = word;
+				} else {
+					cost = atoi(word.c_str());
+				}
+			}
+			
+			router.ID = routerId;
+			router.Network = networkName;
+			router.Cost = cost;
+			//cout << "rid: " << routerId << " netName: " << networkName << endl;
+			
+		} else {
+			
+			stringstream ss(line);
+			
+			int neighborRouterId = -1;
+			int cost;
+			
+			string word;
+			while (ss >> word) {
+				if (neighborRouterId < 0) {
+					neighborRouterId = atoi(word.c_str());
+				} else {
+					cost = atoi(word.c_str());
+				}
+			}
+			
+			router.ConnectedRouters.push_back(
+				ConnectedRouter(neighborRouterId, cost));
+			//cout << "  neighbor: " << neighborRouterId << endl;
+		}
+	}
+	
+}
 
 int main() {
+	
+	initRouters(g_routers);
+	
     cout << '1'<<endl;
     return 0;
 }
